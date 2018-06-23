c87c1dcc60d121a9ec248d0e8f4d0acfc9f77fda
Stas Stahma <StasStahma@gmail.com>
2018-06-23 20:12:26 +0300

    Init commit

---
+++ app/src/main/java/com/example/stas/crimeintent/CrimeListActivity.java
@@ -0,0 +1,10 @@
+package com.example.stas.crimeintent;
+
+import android.support.v4.app.Fragment;
+
+public class CrimeListActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeListFragment();
+    }
+}
